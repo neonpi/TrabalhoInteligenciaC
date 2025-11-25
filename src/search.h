aaacad5f97810381a7f33133626f144dd9ef5b70
neonpi <ninaferreira534@gmail.com>
2025-11-25 18:20:22 -0300

    Adiciona as funções no search.h

--- src/search.h
+++ src/search.h
@@ -7,3 +7,17 @@
 
 #endif //TRABALHO_INTELIGENCIA_COMPUTACIONAL_SEARCH_H
 
+class Search {
+public:
+    Search(Instance* instance);
+    ~Search();
+    void run();
+
+    //Tem que mudar isso aqui, é só pra ter uma base
+    void rvnd_intra();
+    void ls_intra_exchange(bool *improved);
+    void ls_intra_2opt(bool *improved);
+    void ls_intra_or_opt_1(bool *improved);
+    void ls_intra_or_opt_k(int k, bool *improved);
+
+};
