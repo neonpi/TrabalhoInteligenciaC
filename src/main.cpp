bd2fb66acb1dae8688442ab3ab41959fafdf9ffc
Samuel Nascimento <me@samnsc.com>
2025-11-26 00:19:46 -0300

    feat: update main.cpp and CMakeLists.txt to include new classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
+#include <filesystem>
 #include <iostream>
+#include "dependency_graph.hpp"
 
-int main() {
-    std::cout << "Hello World!";
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Too many parameters were supplied.\n";
+
+        return 1;
+    }
+
+    std::filesystem::path instance_file_path{argv[1]};
+    DependencyGraph dependency_graph{instance_file_path};
 
     return 0;
 }
